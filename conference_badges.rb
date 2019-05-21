4ab23d788ca756e7db3c33cf8d928519247d8434
Jason McGloin <jason.mcgloin@gmail.com>
2019-05-21 14:07:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def batch_badge_creator(names)
   names.map {|name| "Hello, my name is " + name + "."}
 end
 
-def assign_rooms(stuff)
+def assign_rooms(names)
   # binding.pry
   names.map {|name, room| "Hello, " + name +"! You'll be assigned to room " + (room + 1).to_s + "!"}
 end
